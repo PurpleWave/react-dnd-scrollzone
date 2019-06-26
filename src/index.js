b0343a06daa56f15b1135b6b81adbd10372c38ad
Alex Neises <alex.neises@purplewave.com>
2019-06-26 10:02:42 -0500

    Attempt to fix unrecognized prop

--- src/index.js
+++ src/index.js
@@ -244,7 +244,7 @@ export default function createScrollingComponent(WrappedComponent) {
         verticalStrength,
         horizontalStrength,
         onScrollChange,
-
+        dragDropManager,
         ...props,
       } = this.props;
 
